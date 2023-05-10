a8e6962f56152e35ea4f7a54cdeea31ce24dd494
Sdney Lima <lima.rosimao@gmail.com>
2023-05-10 10:30:22 -0300

    style/chor: footer and navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as Timeline} from './Timeline'
 export { default as dayPerio} from './DayPeriod'
+// export { default as headers} from './headers.css'
+export { default as ClimateNow} from './ClimateNow.tsx'
 
 
 
