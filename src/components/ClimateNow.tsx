296abfeb4874b3f466ffd85b76f971ac4b7a8736
Sdney Lima <lima.rosimao@gmail.com>
2023-05-10 10:42:33 -0300

    feat: implemented logic for climateNow

---
+++ src/components/ClimateNow.tsx
@@ -0,0 +1,23 @@
+// import './climateNow.css'
+
+// const ClimateNow = () => {
+
+//       async function getCurrentWeather(): Promise<String>{
+//         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=New York&appid={YOUR_API_KEY}&units=metric');
+//         const weatherData = await response.json();
+//         const currentTemp = weatherData.main.temp;
+//         const description = weatherData.weather[0].description;
+        
+//       }
+      
+//       return (
+
+//         <div>
+//           <h1></h1>
+//         </div>
+//       )
+
+//     }
+  
+
+//   export default ClimateNow
